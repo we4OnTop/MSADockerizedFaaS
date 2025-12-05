96c4eb4973e30755bd33b4cc5710bf8916df1f52
we4 <luxivi1324@gmail.com>
2025-12-05 10:48:13 +0100

    imports working but application not fixed

--- envoy/xDS/envoyXDsConnector.go
+++ envoy/xDS/envoyXDsConnector.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net"
 	"time"
 
+	"github.com/envoyproxy/go-control-plane/envoy/api/v2/listener"
 	cluster "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
 	core "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	endpoint "github.com/envoyproxy/go-control-plane/envoy/config/endpoint/v3"
-	listener "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
 	route "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
+	discoverygrpc "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
+	"github.com/envoyproxy/go-control-plane/pkg/cache/types"
+	"github.com/envoyproxy/go-control-plane/pkg/cache/v3"
+	"github.com/envoyproxy/go-control-plane/pkg/resource/v3"
+	serverv3 "github.com/envoyproxy/go-control-plane/pkg/server/v3"
+	"github.com/envoyproxy/go-control-plane/pkg/wellknown"
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/anypb"
 )
@@ -42,7 +49,7 @@ func socketAddress(ip string, port uint32) *core.Address {
 
 // Create a static cluster with a list of hosts
 func newStaticCluster(name string, hosts []string, port uint32) *cluster.Cluster {
-	lbEndpoints := []*endpoint.LbEndpoint{}
+	var lbEndpoints []*endpoint.LbEndpoint
 
 	for _, h := range hosts {
 		lbEndpoints = append(lbEndpoints, &endpoint.LbEndpoint{
@@ -84,7 +91,7 @@ func newRouteConfig(routeName, clusterName string) *route.RouteConfiguration {
 							PathSpecifier: &route.RouteMatch_Prefix{Prefix: "/"},
 						},
 						Action: &route.Route_Route{
-							Route: &route.Route_RouteAction{
+							Route: &route.RouteAction{
 								Cluster: clusterName,
 							},
 						},
@@ -96,6 +103,59 @@ func newRouteConfig(routeName, clusterName string) *route.RouteConfiguration {
 }
 
 // Create a listener with HTTP Connection Manager
+func addNewCluster(nodeID string, name string, hosts []string, port uint32) error {
+	// 1. Get the existing snapshot
+	// Ensure 'snap' is explicitly defined if your IDE is confused
+	snap, err := snapshotCache.GetSnapshot(nodeID)
+	if err != nil {
+		// If no snapshot exists, we might need to create a fresh one
+		return fmt.Errorf("no snapshot found for node %s: %v", nodeID, err)
+	}
+
+	// 2. Prepare the new Cluster
+	newCluster := newStaticCluster(name, hosts, port)
+
+	// 3. READ: Use 'types' (Integer) to get existing resources from the array
+	//    snap.Resources is an array [8]VersionedResources, indexed by INT.
+	existingClusters := snap.GetResources(resource.ClusterType)
+	existingRoutes := snap.GetResources(resource.RouteType)
+	existingListeners := snap.GetResources(resource.ListenerType)
+
+	// 4. Convert Maps to Slices (Snapshot requires slices)
+	//    We append our new cluster to the existing list here.
+	var clusters []types.Resource
+	for _, c := range existingClusters {
+		clusters = append(clusters, c)
+	}
+	clusters = append(clusters, newCluster)
+
+	var routes []types.Resource
+	for _, r := range existingRoutes {
+		routes = append(routes, r)
+	}
+
+	var listeners []types.Resource
+	for _, l := range existingListeners {
+		listeners = append(listeners, l)
+	}
+
+	// 5. WRITE: Use 'resource' (String) to create the new snapshot
+	//    NewSnapshot expects a map keyed by STRING (Type URL).
+	newSnap, err := cache.NewSnapshot(
+		fmt.Sprintf("%d", time.Now().UnixNano()), // Version must be unique
+		map[resource.Type][]types.Resource{
+			resource.ClusterType:  clusters,
+			resource.RouteType:    routes,
+			resource.ListenerType: listeners,
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	return snapshotCache.SetSnapshot(context.Background(), nodeID, newSnap)
+}
+
 func newListener(name, routeName string, port uint32) *listener.Listener {
 	hcm := &listener.HttpConnectionManager{
 		StatPrefix: "ingress_http",
@@ -149,19 +209,41 @@ func setInitialSnapshot() error {
 	return snapshotCache.SetSnapshot(context.Background(), nodeID, snap)
 }
 
-// Add a new cluster dynamically
 func addNewCluster(name string, hosts []string, port uint32) error {
-	snap, _ := snapshotCache.GetSnapshot(nodeID)
+	// 1. Get the existing snapshot
+	snap, err := snapshotCache.GetSnapshot(nodeID)
+	if err != nil {
+		// Handle case where snapshot doesn't exist yet (init new one)
+		// For now, we assume it exists or you return error
+		return err
+	}
 
+	// 2. Prepare the new Cluster
 	newCluster := newStaticCluster(name, hosts, port)
-	clusters := append(snap.Resources[resource.ClusterType], newCluster)
 
+	// 3. Extract EXISTING clusters from the map and convert to Slice
+	//    Note: snap.Resources[type].Items is a map[string]types.Resource
+	var clusters []types.Resource
+	for _, cl := range snap.GetResources(resource.ClusterType) {
+		clusters = append(clusters, cl)
+	}
+
+	// 4. Append the NEW cluster
+	clusters = append(clusters, newCluster)
+
+	// 5. You must also preserve Routes and Listeners (convert them to slices too)
+	//    or they will be deleted in the new snapshot!
+	routes := mapToSlice(snap.GetResources(resource.RouteType))
+	listeners := mapToSlice(snap.GetResources(resource.ListenerType))
+
+	// 6. Create the New Snapshot
+	//    NewSnapshot expects map[resource.Type][]types.Resource
 	newSnap, err := cache.NewSnapshot(
-		"2",
+		"2", // Ideally, increment this version number dynamically
 		map[resource.Type][]types.Resource{
 			resource.ClusterType:  clusters,
-			resource.RouteType:    snap.Resources[resource.RouteType],
-			resource.ListenerType: snap.Resources[resource.ListenerType],
+			resource.RouteType:    routes,
+			resource.ListenerType: listeners,
 		},
 	)
 	if err != nil {
@@ -169,6 +251,15 @@ func addNewCluster(name string, hosts []string, port uint32) error {
 	}
 
 	return snapshotCache.SetSnapshot(context.Background(), nodeID, newSnap)
+}
+
+// Helper to convert the internal Map to a Slice
+func mapToSlice(items map[string]types.Resource) []types.Resource {
+	var out []types.Resource
+	for _, item := range items {
+		out = append(out, item)
+	}
+	return out
 }
 
 // ---------------- Main ----------------
@@ -180,7 +271,7 @@ func main() {
 	// Start xDS server
 	srv := serverv3.NewServer(context.Background(), snapshotCache, nil)
 	grpcServer := grpc.NewServer()
-	serverv3.RegisterServer(grpcServer, srv)
+	discoverygrpc.RegisterAggregatedDiscoveryServiceServer(grpcServer, srv)
 
 	// Set initial snapshot
 	if err := setInitialSnapshot(); err != nil {
@@ -190,7 +281,7 @@ func main() {
 	// Optionally, add a new cluster dynamically after startup
 	go func() {
 		time.Sleep(5 * time.Second)
-		if err := addNewCluster("payments_cluster", []string{"127.0.0.2"}, 9090); err != nil {
+		if err := addNewCluster(nodeID, "payments_cluster", []string{"127.0.0.2"}, 9090); err != nil {
 			log.Printf("Failed to add new cluster: %v", err)
 		} else {
 			log.Println("New cluster 'payments_cluster' added")
